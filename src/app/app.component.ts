fce7fdb6cf7f70c88271a4a00d99f07f76840522
Franklin <fvolgan@gmail.com>
2022-12-15 15:43:26 -0400

    functional menu scroll with links without routerlink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { ViewportScroller } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,35 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+
+export class AppComponent{
   title = 'energin_site';
+
+
+  /*$(window).scroll(function(){
+    var scroll: $(window).scrollTop();
+
+    if(scroll > 100){
+      $('.menu').addClass('.menu-secundario');
+    }else{
+      $('.menu').removeClass('.menu-secundario');
+    }
+  });*/
+
+  function(){
+    var menu: any = document.getElementById('menus');
+    window.addEventListener('scroll', function(){
+      if (this.window.scrollY > 10) menu.classList.add('menu-secundario');
+      else menu.classList.remove('menu-secundario');
+      console.log("scrollado");
+    });
+  };
+
+  
+  constructor (private viewportScroller: ViewportScroller){};
+  scrollTo() {
+    this.viewportScroller.scrollToAnchor('projetos');
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,19 @@ import {MatIconModule} from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ElectricalComponent } from './electrical/electrical/electrical.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ElectricalComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatIconModule
+    MatIconModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
